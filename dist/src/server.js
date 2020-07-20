7398ebb7bc085c33f0922c55f6e35e3b138095cb
Chinedu <chinedu@verifi.ng>
2020-07-20 16:37:46 +0100

    add dist file

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,5 +1,6 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("../app");
-const appServer = new app_1.default();
-appServer.start(5000);
+exports.__esModule = true;
+var app_1 = require("../app");
+var appServer = new app_1["default"]();
+var port = process.env.PORT || '2500';
+appServer.start(parseInt(port));
