5ffc617eedb75539bd50eef84aa5b21927e20fcb
Chinedu <chinedu@verifi.ng>
2020-07-27 18:34:02 +0100

    add reminder check

--- dist/controllers/RequestController.js
+++ dist/controllers/RequestController.js
@@ -263,7 +263,7 @@ let RequestController = class RequestController extends AbstractController_1.Abs
         });
     }
     remindBuster(req, res) {
-        var _a, _b;
+        var _a, _b, _c;
         return tslib_1.__awaiter(this, void 0, void 0, function* () {
             try {
                 const request = yield Request_1.Request.findOne({
@@ -276,9 +276,9 @@ let RequestController = class RequestController extends AbstractController_1.Abs
                 const notification = new NotificationsService_1.default();
                 if (!request)
                     throw new Error('invalid request selected for reminder');
-                if ((_a = request.acceptedBy) === null || _a === void 0 ? void 0 : _a.notificationTokens.length)
+                if ((_b = (_a = request.acceptedBy) === null || _a === void 0 ? void 0 : _a.notificationTokens) === null || _b === void 0 ? void 0 : _b.length)
                     yield notification.sendPushNotification("pickup reminder", `${request.requestedBy.firstName} has sent a pickup reminder`, request.acceptedBy.notificationTokens);
-                if ((_b = request.requestedBy) === null || _b === void 0 ? void 0 : _b.notificationTokens.length)
+                if ((_c = request.requestedBy) === null || _c === void 0 ? void 0 : _c.notificationTokens.length)
                     yield notification.sendPushNotification("pickup reminder", `reminder sent`, request.requestedBy.notificationTokens);
                 res.status(200).json({ success: true, message: "sent reminder" });
             }
